8f68471d1217e47e0f4a9836cf2a88c09edbda91
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-01 13:24:53 +0200

    wrote a function that reads information from package.json

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const contentJSON = fs.readFileSync("./package.json", { encoding: "utf8" });
+const content = JSON.parse(contentJSON);
+console.log(`name: ${content.name}`);
+console.log(`version: ${content.version}`);
+console.log(`number of dependencies: ${content.dependencies ? Object.keys(content.dependencies).length : "0"}`)
+console.log(`number of devDependencies: ${content.devDependencies ? Object.keys(content.devDependencies).length : "0"}`)
+
+const node_stat = fs.statSync('./node_modules');
+const size = node_stat.size;
+console.log(`node_modules: ${size} bytes`);
