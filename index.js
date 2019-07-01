a5a2830f549e6543c5dcc18a9c0d0d316516c8e1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-01 14:10:22 +0200

    changed check for dep and devDep

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const contentJSON = fs.readFileSync("./package.json", { encoding: "utf8" });
 const content = JSON.parse(contentJSON);
 console.log(`name: ${content.name}`);
 console.log(`version: ${content.version}`);
-console.log(`number of dependencies: ${content.dependencies ? Object.keys(content.dependencies).length : "0"}`)
-console.log(`number of devDependencies: ${content.devDependencies ? Object.keys(content.devDependencies).length : "0"}`)
+console.log(`number of dependencies: ${"dependencies" in content ? Object.keys(content.dependencies).length : "0"}`)
+console.log(`number of devDependencies: ${"devDependencies" in content ? Object.keys(content.devDependencies).length : "0"}`)
 
 const node_stat = fs.statSync('./node_modules');
 const size = node_stat.size;
